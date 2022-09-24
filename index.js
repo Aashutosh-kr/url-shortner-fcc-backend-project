8337799cfe2c37bdc05f79f53bd553c4f63b2346
Aashutosh <aashutoshkumar869@gmail.com>
2022-09-24 23:17:18 +0530

    refactored code to limit run time

--- index.js
+++ index.js
@@ -18,10 +18,19 @@ app.get("/", function (req, res) {
 	res.sendFile(process.cwd() + "/views/index.html");
 });
 
+let short_url;
+let num = async () => {
+	short_url = await Url.countDocuments();
+	console.log("short_url", short_url);
+};
+num();
+
 // API for Shortening a url
 app.post("/api/shorturl", async (req, res) => {
 	const { url } = req.body;
-	const domain = url.split("//");
+
+	const domain = url.split("//")[1]?.split("/?")[0];
+	const dnsCheck = dns.lookup(domain, async (err) => (err ? false : true));
 
 	try {
 		const check = url.includes("http://") || url.includes("https://");
@@ -31,21 +40,24 @@ app.post("/api/shorturl", async (req, res) => {
 				message: "prefix url with http:// or https://",
 			});
 		}
-
 		const oldUrl = await Url.findOne({ input_url: url });
-		return oldUrl
-			? res.json({
-					original_url: oldUrl.input_url,
-					short_url: oldUrl.short_url,
-			  })
-			: dns.lookup(domain[2], async (err) => {
-					return err
-						? res.json({ error: "invalid URL" })
-						: await Url.create({
-								input_url: url,
-								short_url: (await Url.find().countDocuments()) + 1,
-						  });
-			  });
+
+		if (oldUrl) {
+			return res.json({
+				original_url: oldUrl.input_url,
+				short_url: oldUrl.short_url,
+			});
+		}
+		if (dnsCheck) {
+			short_url++;
+			const newUrl = await Url.create({
+				input_url: url,
+				short_url,
+			});
+			return res.json({ original_url: newUrl.input_url, short_url });
+		} else {
+			return res.json({ error: "invalid URL" });
+		}
 	} catch (error) {
 		console.error(error.message);
 		return res.json({ err: error.message });
