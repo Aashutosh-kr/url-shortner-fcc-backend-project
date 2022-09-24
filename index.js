11c61b11d7f906c8b95a911b598351e62412ccec
Aashutosh <aashutoshkumar869@gmail.com>
2022-09-24 15:56:57 +0530

    added dns lookup to check for valid urls with http or https

--- index.js
+++ index.js
@@ -21,22 +21,31 @@ app.get("/", function (req, res) {
 // API for Shortening a url
 app.post("/api/shorturl", async (req, res) => {
 	const { url } = req.body;
+	const domain = url.split("//");
+
 	try {
-		dns.lookup(url, async (err) => {
-			if (err) {
-				return res.json({ error: "invalid URL" });
-			}
-			const oldUrl = await Url.findOne({ input_url: url });
-			return oldUrl
-				? res.json({
-						original_url: oldUrl.input_url,
-						short_url: oldUrl.short_url,
-				  })
-				: await Url.create({
-						input_url: url,
-						short_url: (await Url.find().countDocuments()) + 1,
-				  });
-		});
+		const check = url.includes("http://") || url.includes("https://");
+		if (!check) {
+			return res.json({
+				error: "invalid url",
+				message: "prefix url with http:// or https://",
+			});
+		}
+
+		const oldUrl = await Url.findOne({ input_url: url });
+		return oldUrl
+			? res.json({
+					original_url: oldUrl.input_url,
+					short_url: oldUrl.short_url,
+			  })
+			: dns.lookup(domain[2], async (err) => {
+					return err
+						? res.json({ error: "invalid URL" })
+						: await Url.create({
+								input_url: url,
+								short_url: (await Url.find().countDocuments()) + 1,
+						  });
+			  });
 	} catch (error) {
 		console.error(error.message);
 		return res.json({ err: error.message });
